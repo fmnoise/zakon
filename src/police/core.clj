4aaf3c0afa194f83ee5bbb7fa559e889db2f3c66
fmnoise <fmnoise@gmail.com>
2018-04-25 14:36:42 +0300

    Add policy as a dispatch argument

--- src/police/core.clj
+++ src/police/core.clj
@@ -1,16 +1,19 @@
 (ns police.core)
 
-;; XXX add docstrings
-
-(def any ::any) ;; ??? :actor/any :action/any :subject/any
-
-(def entities (atom (make-hierarchy)))
-
+(def any ::any)
+(def global-policy ::policy)
+(def relations (atom (make-hierarchy)))
 (def ^:dynamic *default-result* false)
+(def ^:dynamic *policy* global-policy)
+(def ^:dynamic *actor-dispatcher* identity)
+(def ^:dynamic *action-dispatcher* identity)
+(def ^:dynamic *subject-dispatcher* identity)
 
 (defn ^:dynamic *dispatcher*
   [actor action subject]
-  [actor action subject])
+  [(*actor-dispatcher* actor)
+   (*action-dispatcher* action)
+   (*subject-dispatcher* subject)])
 
 (defn set-default-result! [res]
   (alter-var-root #'*default-result* (constantly res)))
@@ -19,23 +22,31 @@
   (alter-var-root #'*dispatcher* (constantly f)))
 
 (defmulti dispatch
-  (fn [actor action subject] [actor action subject])
-  :hierarchy entities) ;; #'dispatcher
+  (fn [policy actor action subject] [policy actor action subject])
+  :hierarchy relations)
 
-(defmethod dispatch [any any any] [_ _ _] {:result *default-result*
-                                           :source ::default})
+(defmethod dispatch [global-policy any any any]
+  [_ _ _ _] {:result *default-result*
+             :source ::default-rule})
 
-(defn- known? [s]
-  (or (= s any)
-      (contains? (descendants @entities any) s)))
+(defn- known-entity? [entity]
+  (or (= entity any)
+      (contains? (descendants @relations any) entity)))
 
-(defn- register! [kw]
-  (let [ns (namespace kw)
+(defn- known-policy? [policy]
+  (or (= policy global-policy)
+      (contains? (descendants @relations global-policy) policy)))
+
+(defn- register-entity! [entity]
+  (let [ns (namespace entity)
         str-any (name any)
-        kw-any (keyword ns str-any)]
-    (when-not (known? kw-any)
-      (swap! entities derive kw-any any))
-    (swap! entities derive kw kw-any)))
+        entity-any (keyword ns str-any)]
+    (when-not (known-entity? entity-any)
+      (swap! relations derive entity-any any))
+    (swap! relations derive entity entity-any)))
+
+(defn- register-policy! [policy]
+  (swap! relations derive policy global-policy))
 
 (defn make-kw [v]
   (let [kw (cond
@@ -45,72 +56,113 @@
         nsp (or (namespace kw) (str *ns*))]
     (keyword nsp (name kw))))
 
-(defn inherit! [child parent]
+(defn inherit!
+  [child parent]
   (let [kw-child (make-kw child)
         kw-parent (make-kw parent)
-        _ (when-not (known? kw-parent) (register! kw-parent))]
-    (swap! entities derive kw-child kw-parent)))
+        _ (when-not (known-entity? kw-parent) (register-entity! kw-parent))]
+    (swap! relations derive kw-child kw-parent)))
 
 (defn inherited? [child parent]
   (let [kw-child (make-kw child)
         kw-parent (make-kw parent)]
-    (-> @entities
+    (-> @relations
         (descendants kw-parent)
         (contains? kw-child))))
 
-(defn can? [actor action subject]
-  (let [[actor' action' subject'] (*dispatcher* actor action subject)
-        kw-actor (make-kw actor')
-        kw-action (make-kw action')
-        kw-subject (make-kw subject')
-        _ (when-not (known? kw-actor) (register! kw-actor))
-        _ (when-not (known? kw-action) (register! kw-action))
-        _ (when-not (known? kw-subject) (register! kw-subject))
-        {:keys [result]} (dispatch kw-actor kw-action kw-subject)]
-    (if (ifn? result)
-      (result [actor action subject])
-      result)))
-
-(defn describe [actor action subject]
-  (let [[actor' action' subject'] (*dispatcher* actor action subject)
-        kw-actor (make-kw actor')
-        kw-action (make-kw action')
-        kw-subject (make-kw subject')
-        _ (when-not (known? kw-actor) (register! kw-actor))
-        _ (when-not (known? kw-action) (register! kw-action))
-        _ (when-not (known? kw-subject) (register! kw-subject))
-        {:keys [source]} (dispatch kw-actor kw-action kw-subject)]
-    source))
+(defn can?
+  ([actor action subject]
+   (can? *policy* actor action subject))
+  ([policy actor action subject]
+   (let [[actor' action' subject'] (*dispatcher* actor action subject)
+         kw-actor (make-kw actor')
+         kw-action (make-kw action')
+         kw-subject (make-kw subject')
+         kw-policy (make-kw policy)
+         _ (when-not (known-entity? kw-actor) (register-entity! kw-actor))
+         _ (when-not (known-entity? kw-action) (register-entity! kw-action))
+         _ (when-not (known-entity? kw-subject) (register-entity! kw-subject))
+         _ (when-not (known-policy? kw-policy) (register-policy! kw-policy))
+         {:keys [result]} (dispatch kw-policy kw-actor kw-action kw-subject)]
+     (if (ifn? result)
+       (result [actor action subject])
+       result))))
 
 (def cant? (complement can?))
 
+(defn find-rule
+  ([actor action subject]
+   (find-rule *policy* actor action subject))
+  ([policy actor action subject]
+   (let [[actor' action' subject'] (*dispatcher* actor action subject)
+         kw-actor (make-kw actor')
+         kw-action (make-kw action')
+         kw-subject (make-kw subject')
+         kw-policy (make-kw policy)
+         _ (when-not (known-entity? kw-actor) (register-entity! kw-actor))
+         _ (when-not (known-entity? kw-action) (register-entity! kw-action))
+         _ (when-not (known-entity? kw-subject) (register-entity! kw-subject))
+         _ (when-not (known-policy? kw-policy) (register-policy! kw-policy))
+         {:keys [source]} (dispatch kw-policy kw-actor kw-action kw-subject)]
+     source)))
+
 (defmacro defrule
-  [[actor action subject] res]
-  (let [m (meta &form)
-        ns (str *ns*)]
-   `(let [kw-actor# (make-kw ~actor)
-          kw-action# (make-kw ~action)
-          kw-subject# (make-kw ~subject)
-          prev-rules# (-> dispatch prefers keys)]
-      (defmethod dispatch [kw-actor# kw-action# kw-subject#]
-        [~'_ ~'_ ~'_]
-        {:result ~res
-         :source (assoc ~m :ns ~ns)})
-      (if (empty? prev-rules#)
-        (prefer-method dispatch [kw-actor# kw-action# kw-subject#] any)
-        (doseq [rule# prev-rules#]
-          (when-not (= rule# [kw-actor# kw-action# kw-subject#])
-            (prefer-method dispatch [kw-actor# kw-action# kw-subject#] rule#))))
-      nil)))
+  ([rule res]
+   (let [m (meta &form)]
+     `~(with-meta
+         `(defrule *policy* ~rule ~res)
+         m)))
+  ([policy [actor action subject] res]
+   (let [m (meta &form)
+         ns (str *ns*)]
+    `(let [kw-actor# (make-kw ~actor)
+           kw-action# (make-kw ~action)
+           kw-subject# (make-kw ~subject)
+           kw-policy# (make-kw ~policy)
+           new-rule# [kw-policy# kw-actor# kw-action# kw-subject#]
+           prev-rules# (-> dispatch prefers keys set (conj any))]
+       (defmethod dispatch [kw-policy# kw-actor# kw-action# kw-subject#]
+         [~'_ ~'_ ~'_ ~'_]
+         {:result ~res
+          :source (assoc ~m :ns ~ns)})
+       (if (contains? prev-rules# new-rule#)
+         nil
+         (do
+          (doseq [rule# prev-rules#]
+            (prefer-method dispatch new-rule# rule#))
+          {:policy kw-policy#
+           :actor kw-actor#
+           :action kw-action#
+           :subject kw-subject#
+           :result ~res}))))))
 
-(defmacro can! [actor action subject]
-  (let [m (meta &form)]
-    `~(with-meta
-       `(defrule [~actor ~action ~subject] true)
-        m)))
+(defmacro can!
+  ([actor action subject]
+   (let [m (meta &form)]
+     `~(with-meta
+         `(can! *policy* ~actor ~action ~subject)
+         m)))
+  ([policy actor action subject]
+   (let [m (meta &form)]
+     `~(with-meta
+         `(defrule ~policy [~actor ~action ~subject] true)
+         m))))
 
-(defmacro cant! [actor action subject]
+(defmacro cant!
+  ([actor action subject]
+   (let [m (meta &form)]
+     `~(with-meta
+         `(cant! *policy* ~actor ~action ~subject)
+         m)))
+  ([policy actor action subject]
+   (let [m (meta &form)]
+     `~(with-meta
+         `(defrule ~policy [~actor ~action ~subject] false)
+         m))))
+
+(defmacro with-policy [policy & body]
   (let [m (meta &form)]
-    `~(with-meta
-        `(defrule [~actor ~action ~subject] false)
-        m)))
+    `(binding [*policy* ~policy]
+       ~(with-meta
+          `(do ~@body)
+          m))))
