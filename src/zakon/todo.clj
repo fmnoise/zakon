71ab449dfddfb8b048c2806fec68be96a0d92837
fmnoise <fmnoise@gmail.com>
2018-05-01 13:41:53 +0300

    Add some todos

--- src/zakon/todo.clj
+++ src/zakon/todo.clj
@@ -2,7 +2,8 @@
 
 ;; XXX with-dispatchers {:actor (fn []) :subject (fn [])}
 ;; XXX load-policy for loading from plain values (or db)
-;; (load-policy {:policy-name [[:user :do :stuff true] [:user :sell :goods false]]})
+`(load-policy {:policy-name [[:user :do :stuff true] [:user :sell :goods false]]})
+;; ??? how to serialize functions and keywords properly to db
 
 ;; ??? separate :actor/any :action/any :subject/any
 ;; ??? (defrule->> res [a b c] [d e f]
@@ -20,3 +21,18 @@
 
 ;; ??? atom with vector of applied rules
 ;; ??? remove creating any for each ns
+
+;; ??? how to dispatch on set or vector
+'(with-dispatchers {:actor :admin-group :subject :type}
+  (can! :file-admin :delete :file)
+  (can? {:role :admin :admin-groups [:file-admin :site-admin]}
+        :delete
+        {:name "Test" :type :file})) ;; => false :(
+
+;; of course we can
+'(with-dispatchers {:actor :role :subject :type}
+   (defrule [:admin :delete :file]
+     (fn [{:keys [actor]}] (contains? (-> actor :admin-groups set) :file-admin))))
+
+;; ??? what about setting dispatchers as maps
+`(can! {:role :admin} :delete {:type :file})
