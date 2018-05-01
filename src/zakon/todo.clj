20eb8407cfa654e609e0cae7de2056f2b4c1b6fd
fmnoise <fmnoise@gmail.com>
2018-05-01 21:20:38 +0300

    Update todo

--- src/zakon/todo.clj
+++ src/zakon/todo.clj
@@ -1,27 +1,34 @@
 (ns zakon.todo)
 
-;; XXX with-dispatchers {:actor (fn []) :subject (fn [])}
-;; XXX load-policy for loading from plain values (or db)
-`(load-policy {:policy-name [[:user :do :stuff true] [:user :sell :goods false]]})
-;; ??? how to serialize functions and keywords properly to db
-
 ;; ??? separate :actor/any :action/any :subject/any
-;; ??? (defrule->> res [a b c] [d e f]
-;; ??? (can->> [a b c] [d e f] [g h j])
-
+;; ??? atom with vector of applied rules
+;; ??? remove creating any for each ns
+;; ??? caching
+;; --------------------------------------
+;; SUGAR!!!
+'(defrule->> res [a b c] [d e f])
+'(can->> [a b c] [d e f] [g h j])
+;; --------------------------------------
+;; ??? load-policy for loading from plain values (or db)
+`(load-policy {:policy-name [[:user :do :stuff true] [:user :sell :goods false]]})
+;; ^^^ we don't need that, as it will make whole lib too opionated
+;; let's leave it here as a plan, maybe let's create separate ns for experimenting
+;; ??? how to serialize functions and keywords properly to db
+;; ^^^ not our problem indeed
+;; --------------------------------------
+;; TOO MUCH SUGAR...
 ;; not sure we need all that stuff
-;; ??? (can-> {:actor :user} [:play :games] [:watch :tv])
-;; ??? (can-> {:action :play} [:user :games] [:child :toys])
-;; ??? (can-> {:subject :games} [:user :play] [:child :watch])
-;; ??? (can-> {:actor :user :subject :games} :play :watch)
-;; ??? (can-some? {:actor (User. 1)} [:go :home] [:play :football])
+'(can-> {:actor :user} [:play :games] [:watch :tv])
+'(can-> {:action :play} [:user :games] [:child :toys])
+'(can-> {:subject :games} [:user :play] [:child :watch])
+'(can-> {:actor :user :subject :games} :play :watch)
+'(can-some? {:actor (User. 1)} [:go :home] [:play :football])
+;; --------------------------------------
 ;; ??? readers
 ;; #policy [[:user :do :stuff true] [:user :do :other false]]
 ;; #rule [:user :do :stuff false]
-
-;; ??? atom with vector of applied rules
-;; ??? remove creating any for each ns
-
+;; ^^^ not really sure about that
+;; --------------------------------------
 ;; ??? how to dispatch on set or vector
 '(with-dispatchers {:actor :admin-group :subject :type}
   (can! :file-admin :delete :file)
@@ -33,6 +40,8 @@
 '(with-dispatchers {:actor :role :subject :type}
    (defrule [:admin :delete :file]
      (fn [{:keys [actor]}] (contains? (-> actor :admin-groups set) :file-admin))))
-
+;; ^^^ let's stick with that
+;; --------------------------------------
 ;; ??? what about setting dispatchers as maps
 `(can! {:role :admin} :delete {:type :file})
+;; ^^^ probably redundant sugar
