eb369ad22ccef32469299ff1c8ee7bd57bfffe93
fmnoise <fmnoise@gmail.com>
2018-07-09 11:44:01 +0300

    Fix project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject zakon "0.1.0"
   :description "Authorization library"
-  :url "http://github.com/dawcs/police"
+  :url "http://github.com/dawcs/zakon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
